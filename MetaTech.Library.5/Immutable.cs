ecb4081b1da2d8abfdfd90e3ea05fd7a59db26fa
Serj-Tm <serj-tm@ya.ru>
2015-06-20 01:55:01 +0300

    Else_Empty ImmutableArray ImmutableList

--- MetaTech.Library.5/Immutable.cs
+++ MetaTech.Library.5/Immutable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace MetaTech.Library
 {
     public static class Immutable
     {
+      public static ImmutableArray<T> Else_Empty<T>(this ImmutableArray<T>? items)
+      {
+        return items ?? ImmutableArray<T>.Empty;
+      }
+      public static ImmutableList<T> Else_Empty<T>(this ImmutableList<T> items)
+      {
+        return items ?? ImmutableList<T>.Empty;
+      }
     }
 }
