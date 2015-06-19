7b79ad52db5676a2363a9b81f1cba5a5cc5e4615
Serj-Tm <serj-tm@ya.ru>
2015-06-20 02:11:59 +0300

    в догонку

---
+++ MetaTech.Library.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaTech.Library
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (true)
+      {
+        ImmutableArray<int>? items = null;
+        var items2 = items.Else_Empty();
+        foreach (var item in items2)
+        {
+          Console.WriteLine(item);
+        }
+      }
+      if (true)
+      {
+        ImmutableList<int> items = null;
+        var items2 = items.Else_Empty();
+        foreach (var item in items2)
+        {
+          Console.WriteLine(item);
+        }
+      }
+    }
+  }
+}
